547297181182b2312e86e3636f017bb8bc0fd44f
zeyanwang7 <147487545+zeyanwang7@users.noreply.github.com>
2026-04-10 09:26:39 +0800

    Delete 论文数据处理 directory

--- 论文数据处理/基础数据处理/test112.py
+++
@@ -1,6 +0,0 @@
-import geopandas as gpd
-shp_path = r'E:\数据集\中国_省\中国_省2.shp'
-china_map = gpd.read_file(shp_path, encoding='utf-8')
-# 看看前10个地图里的原始名字
-print("SHP原始名称样例：")
-print(china_map['name'].head(10).tolist())
